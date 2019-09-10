105920cf6730a7eda53cb059c31f9608c5e8d711
Thanasis Katsadas <thanasis00@gmail.com>
2019-09-10 17:07:09 +0300

    Update i3_bootstrap.sh

--- i3_bootstrap.sh
+++ i3_bootstrap.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 
 # Essentials
-sudo apt install sudo git xorg pulseaudio vim curl make gcc vim-gtk htop # vim-gtk is the +clipboard version
-sudo apt install transmission blueman evince thunderbird pidgin vlc
-sudo apt install imagemagick scrot sxiv pkg-config libncursesw5-dev feh
+sudo apt install -y sudo git xorg pulseaudio vim curl make gcc vim-gtk htop # vim-gtk is the +clipboard version
+sudo apt install -y transmission blueman evince thunderbird pidgin vlc
+sudo apt install -y imagemagick scrot sxiv pkg-config libncursesw5-dev feh
 
 # NOTE TO SELF: dont mess with fontconfig, it messes up qt5 applications
-sudo apt install i3 libnotify libnotify-bin xorg \
+sudo apt install -y i3 libnotify libnotify-bin xorg \
 	xserver-xorg-input-libinput xserver-xorg-input-evdev \
 	xserver-xorg-input-mouse xbacklight arandr
 
 # Terminal emulator
-sudo apt install gnome-terminal
+sudo apt install -y gnome-terminal
 
 # Compositor
-sudo apt install compton
+sudo apt install -y compton
 
 # Network manager
-sudo apt install network-manager-gnome gnome-keyring # keyring is needed to submit a wifi password
+sudo apt install -y network-manager-gnome gnome-keyring # keyring is needed to submit a wifi password
 
 # GUI to change GTK2 and GTK3 appearance
-sudo apt install lxappearance
+sudo apt install -y lxappearance
 
 # Pulseaudio GUI 
-sudo apt install pavucontrol
+sudo apt install -y pavucontrol
 
 # This handles automounting of devices
-sudo apt install python3-udiskie
+sudo apt install -y python3-udiskie
 
 # GTK3 theme
-sudo apt install arc-theme
+sudo apt install -y arc-theme
 
 # ROFI DEPS
 # Are the deps needed????
-sudo apt install libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev
-sudo apt install rofi
+sudo apt install -y libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev
+sudo apt install -y rofi
 
 # Fonts
-sudo apt install ttf-inconsolata fonts-inconsolata fonts-font-awesome fonts-cantarell
+sudo apt install -y ttf-inconsolata fonts-inconsolata fonts-font-awesome fonts-cantarell
 # Also install chrome
 
 # Xidlehook
-sudo apt install libxss-dev
+sudo apt install -y libxss-dev
 
 # FIRMWARE for bluetooth (on Debian netinst)
-sudo apt install firmware-atheros pulseaudio-module-bluetooth
+sudo apt install -y firmware-atheros pulseaudio-module-bluetooth
 
 # Polybar deps on Debian
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev
+sudo apt-get install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev
 # do the https://github.com/jaagr/polybar/wiki/Compiling
 
 # Needed by battery-dunst
-sudo apt install acpi
+sudo apt install -y acpi
 
 # Set qt5 theme
-sudo apt install qt5ct
+sudo apt install -y qt5ct
 
 # keep touchpad.conf from /etc/X11/xorg.conf.d/30-touchpad.conf
 mkdir Pictures
